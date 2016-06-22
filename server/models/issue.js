69841e8769baf09f5965310b185463d0eb08562e
Ian Shoemaker <ishoemaker@candoris.com>
2016-06-22 16:16:46 -0400

    update issue model

--- server/models/issue.js
+++ server/models/issue.js
@@ -1,18 +1,26 @@
-var mongoose = require('mongoose');
-
 var IssueDefinition = {
   name: 'Issue',
   opts: {
     collection: 'issues'
   },
   schema: {
-    title: String,
-    description: String,
     body: String,
-    createDate: {
-      type: Date,
-      default: Date.now
-    }
+    closed_at: String,
+    comments_url: String,
+    comments: Number,
+    created_at: String,
+    events_url: String,
+    html_url: String,
+    id: Number,
+    labels_url: String,
+    locked: Boolean,
+    milestone: String,
+    number: Number,
+    repository_url: String,
+    state: String,
+    title: String,
+    updated_at: String,
+    url: String
   }
 };
 
